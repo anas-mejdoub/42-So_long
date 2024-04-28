490ca2972b3b008e243670f0d0014fae6d86b1a4
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-04-28 11:05:18 +0100

    parse the extension

--- Bonus/check_extens_bonus.c
+++ Bonus/check_extens_bonus.c
@@ -6,7 +6,7 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:11:39 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/04/26 11:38:45 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/04/28 11:03:07 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	check_extension(char *filename)
 	char	*extension;
 
 	extension = ft_strrchr(filename, '.');
-	if (!ft_strncmp(extension, ".ber", 5))
+	if (!ft_strncmp(extension, ".ber", ft_strlen2(extension)))
 		return (1);
 	write(2, "invalid extension !\n", 21);
 	exit(1);
--- Mandatory/check_extens.c
+++ Mandatory/check_extens.c
@@ -6,7 +6,7 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:26:31 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/04/25 12:48:00 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/04/28 11:04:30 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int check_extension(char *filename)
 {
     char *extension = ft_strrchr(filename, '.');
-    if (!ft_strncmp(extension, ".ber", 5))
+    if (!ft_strncmp(extension, ".ber", ft_strlen2(extension)))
         return (1);
     write(2, "invalid extension !\n", 21);
     exit(1);
